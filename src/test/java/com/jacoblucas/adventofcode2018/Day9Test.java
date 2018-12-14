0295c1e8ab8160f460bb18738e32a79c809eab23
Jacob Lucas <jacob.lucas@gmail.com>
2018-12-13 22:17:47 -0800

    Day 9 part 2 test corrections

--- src/test/java/com/jacoblucas/adventofcode2018/Day9Test.java
+++ src/test/java/com/jacoblucas/adventofcode2018/Day9Test.java
@@ -9,12 +9,12 @@ public class Day9Test {
 
     @Test
     public void testGetHighScore() {
-        assertThat(Day9.getHighScore(9, 25), is(32));
-        assertThat(Day9.getHighScore(10, 1618), is(8317));
-        assertThat(Day9.getHighScore(13, 7999), is(146373));
-        assertThat(Day9.getHighScore(17, 1104), is(2764));
-        assertThat(Day9.getHighScore(21, 6111), is(54718));
-        assertThat(Day9.getHighScore(30, 5807), is(37305));
+        assertThat(Day9.getHighScore(9, 25), is(32L));
+        assertThat(Day9.getHighScore(10, 1618), is(8317L));
+        assertThat(Day9.getHighScore(13, 7999), is(146373L));
+        assertThat(Day9.getHighScore(17, 1104), is(2764L));
+        assertThat(Day9.getHighScore(21, 6111), is(54718L));
+        assertThat(Day9.getHighScore(30, 5807), is(37305L));
     }
 
 }
